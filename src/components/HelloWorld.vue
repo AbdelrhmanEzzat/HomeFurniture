0379df7dbb4bc35455c14e41bd8fa16444fc9726
fatma.fatmssalah04 <fatma.fatmssalah04@gmail.com>
2021-12-11 13:09:33 +0200

    fistUpdate

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,48 +1,114 @@
 <template>
-  <div class="hello">
-    <MDBContainer>
-      <div
-        class="d-flex justify-content-center align-items-center"
-        style="height: 100vh"
-      >
-        <div class="text-center">
-          <img
-            class="mb-4"
-            src="https://mdbootstrap.com/img/logo/mdb-transparent-250px.png"
-            style="width: 250px; height: 90px"
-          />
-          <h5 class="mb-3">{{ msg }}</h5>
-          <p class="mb-3">MDB Team</p>
-          <p class="mt-5 mb-3">
-            PS. We'll be releasing "How to build your first project with MDB 5
-            Vue" tutorial soon. Make sure to join our newsletter not to miss it!
-          </p>
-          <a
-            class="btn btn-primary btn-lg"
-            href="https://mdbootstrap.com/newsletter/"
-            target="_blank"
-            role="button"
-            >Join now</a
-          >
-        </div>
+  <!-- Just an image -->
+  <MDBNavbar light bg="light" container>
+    <MDBNavbarBrand href="#">
+      <img
+        src="https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.png"
+        height="30"
+        alt=""
+        loading="lazy"
+      />
+      <!---------------------- Icons ----------------------------->
+      <div class="sticky-top">
+        <MDBNavbarNav class="d-flex flex-row">
+          <MDBNavbarItem to="#" class="me-3 me-lg-3">
+            <MDBIcon icon="shopping-cart"></MDBIcon>
+          </MDBNavbarItem>
+          <MDBNavbarItem href="#" class="me-3 me-lg-3">
+            <MDBIcon icon="heart" iconStyle="fas"></MDBIcon>
+          </MDBNavbarItem>
+          <!-- Icon dropdown -->
+          <MDBNavbarItem class="me-3 me-lg-3 dropdown">
+            <MDBDropdown v-model="dropdown3">
+              <MDBDropdownToggle
+                tag="a"
+                class="nav-link"
+                @click="dropdown3 = !dropdown3"
+              >
+                <MDBIcon icon="user" />
+              </MDBDropdownToggle>
+              <MDBDropdownMenu>
+                <MDBDropdownItem href="#">Action</MDBDropdownItem>
+                <MDBDropdownItem href="#">Another Action</MDBDropdownItem>
+                <MDBDropdownItem href="#">Something else here</MDBDropdownItem>
+              </MDBDropdownMenu>
+            </MDBDropdown>
+          </MDBNavbarItem>
+        </MDBNavbarNav>
       </div>
-    </MDBContainer>
-  </div>
+      <!----------------------Section----------------------------->
+      <div class="Section">
+        <MDBNavbarNav class="d-flex flex-row">
+          <MDBNavbarItem to="#" class="me-2 me-lg-3" active>
+            Home
+          </MDBNavbarItem>
+          <MDBNavbarItem to="#" class="me-2 me-lg-4"> Sale </MDBNavbarItem>
+          <MDBNavbarItem to="#" class="me-2 me-lg-4"> Pages </MDBNavbarItem>
+          <MDBNavbarItem to="#" class="me-2 me-lg-4"> Blog </MDBNavbarItem>
+          <MDBNavbarItem to="#" class="me-2 me-lg-4"> Elements </MDBNavbarItem>
+          <MDBNavbarItem to="#" class="me-2 me-lg-4"> Features </MDBNavbarItem>
+        </MDBNavbarNav>
+      </div>
+    </MDBNavbarBrand>
+  </MDBNavbar>
 </template>
 
 <script>
-import { MDBContainer } from "mdb-vue-ui-kit";
+import {
+  MDBIcon,
+  MDBNavbar,
+  MDBNavbarNav,
+  MDBNavbarBrand,
+  MDBNavbarItem,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+} from "mdb-vue-ui-kit";
+import { ref } from "vue";
 
 export default {
   name: "HelloWorld",
   components: {
-    MDBContainer,
+    MDBNavbarBrand,
+    MDBIcon,
+    MDBNavbar,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBDropdown,
+    MDBDropdownToggle,
+    MDBDropdownMenu,
+    MDBDropdownItem,
   },
-  props: {
-    msg: String,
+  setup() {
+    const dropdown3 = ref(false);
+
+    return {
+      dropdown3,
+    };
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.sticky-top {
+  width: 10%;
+  position: absolute;
+  top: 8%;
+  left: 90%;
+  border-radius: 4px;
+  /* padding: 1px 5px; */
+}
+/* ul {
+  padding-left: 5px;
+} */
+.Section {
+  width: 40%;
+  position: absolute;
+  top: 15%;
+  left: 50%;
+  border-radius: 4px;
+  font-size: smaller;
+}
+</style>
